c3205881c16dcb25f6dcf7a0c22837b3db6df067
yuumi.mizoi <yuumi.mizoi@gmail.com>
2024-11-28 17:18:06 +0900

    フィードバックに基づいてコードを修正しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,37 +1,32 @@
 #!/usr/bin/env ruby
 require "date"
+require 'optparse'
 
-year = nil
-month = nil
+options = {}
 
-ARGV.each_with_index do |value, index|
-  if value == "-y"
-    year = ARGV[index + 1].to_i if ARGV[index + 1]
-  elsif value == "-m"
-    month = ARGV[index + 1].to_i if ARGV[index + 1]
+OptionParser.new do |opts|
+  opts.on("-y YEAR", " --year YEAR", Integer, "カレンダーの年を指定します") do |year|
+    options[:year] = year
   end
-end
+  opts.on("-m MONTH", "--month MONTH", Integer, "カレンダーの月を指定します") do |month|
+    options[:month] = month
+  end
+end.parse!
 
-year ||= Date.today.year
-month ||= Date.today.month
+year = options[:year] || Date.today.year
+month = options[:month] || Date.today.month
 
 def generate_calendar(year, month)
   first_day = Date.new(year, month, 1)
   last_day = Date.new(year, month, -1)
 
   puts "#{year}年#{month}月".center(20)
-  puts " Su Mo Tu We Th Fr Sa"
+  puts " 日 月 火 水 木 金 土"
   print "   " * first_day.wday
 
-  (1..last_day.day).each do |day|
-    if day < 10
-      print "  #{day}"
-    else
-      print " #{day}"
-    end
-    if (first_day + (day - 1)).wday == 6
-      print "\n"
-    end
+  (first_day..last_day).each do |date|
+    print date.day.to_s.rjust(3)
+    print "\n" if date.saturday?
   end
   print "\n"
 end
