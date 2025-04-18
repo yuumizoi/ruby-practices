7391a918fa11095dc162f103f3cf4af847b2f6d9
yuumizoi <y.mizoi@genbasupport.com>
2025-04-18 13:42:44 +0900

    コード重複していたファイルの削除と、Rubocop-fjordの警告修正対応

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,44 +1,54 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
+score_string = ARGV[0]
+scores = score_string.split(',')
 
 shots = []
 
-scores.each do |score_value|
-  if score_value == 'X'
-    shots << 10
+scores.each do |score_string_item|
+  shots << (score_string_item == 'X' ? 10 : score_string_item.to_i)
+end
+
+frames = []
+index = 0
+
+while index < shots.length && frames.length < 10
+  if frames.length == 9
+    frames << shots[index..]
+    break
+  elsif shots[index] == 10
+    frames << [10]
+    index += 1
   else
-    shots << score_value.to_i
+    frames << [shots[index], shots[index + 1]]
+    index += 2
   end
 end
 
-point = 0
-index = 0
+point = 0 # point の初期化
 
-10.times do |frame_index|
-  if frame_index == 9
-    if shots[index] == 10
-      point += 10 + shots[index + 1] + shots[index + 2]
-    elsif shots[index] + shots[index + 1] == 10
-      point += 10 + shots[index + 2]
-    else
-      point += shots[index] + shots[index + 1]
-    end
-  elsif shots[index] == 10
-    # ストライクの処理
-    point += 10 + shots[index + 1] + shots[index + 2]
-    index += 1
-  elsif shots[index] + shots[index + 1] == 10
-    # スペアの処理
-    point += 10 + shots[index + 2]
-    index += 2
-  else
-    # 通常の処理
-    point += shots[index] + shots[index + 1]
-    index += 2
-  end
+frames.size.times do |i|
+  point += if i <= 8 # 1〜9フレームの処理
+             if frames[i][0] == 10 # ストライク
+               if i + 2 < frames.length && frames[i + 1][0] == 10
+                 10 + frames[i + 1][0] + frames[i + 2][0]
+               else
+                 10 + frames[i + 1][0] + frames[i + 1][1]
+               end
+             elsif frames[i][0] + frames[i][1] == 10 && i + 1 < frames.length
+               10 + frames[i + 1][0]
+             else # 通常フレーム
+               frames[i][0] + frames[i][1]
+             end
+           elsif frames[9][0] == 10
+             # 10フレーム目の処理（i == 9）
+             frames[9][0] + frames[9][1] + frames[9][2]
+           elsif frames[9][0] + frames[9][1] == 10
+             frames[9][0] + frames[9][1] + frames[9][2]
+           else
+             frames[9][0] + frames[9][1]
+           end
 end
 
 puts point
