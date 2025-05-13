c3833e672c506ce5b92e4d65783884ab00facbc5
yuumizoi <y.mizoi@genbasupport.com>
2025-05-13 18:02:09 +0900

    スコア計算処理を簡潔化（map/sumの使用と条件分岐の見直し・整理）

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,11 +4,7 @@
 score_string = ARGV[0]
 scores = score_string.split(',')
 
-shots = []
-
-scores.each do |score_string_item|
-  shots << (score_string_item == 'X' ? 10 : score_string_item.to_i)
-end
+shots = scores.map { |score_string_item| score_string_item == 'X' ? 10 : score_string_item.to_i }
 
 frames = []
 index = 0
@@ -29,26 +25,15 @@ end
 point = 0 # point の初期化
 
 frames.size.times do |i|
-  point += if i <= 8 # 1〜9フレームの処理
-             if frames[i][0] == 10 # ストライク
-               if i + 2 < frames.length && frames[i + 1][0] == 10
-                 10 + frames[i + 1][0] + frames[i + 2][0]
-               else
-                 10 + frames[i + 1][0] + frames[i + 1][1]
-               end
-             elsif frames[i][0] + frames[i][1] == 10 && i + 1 < frames.length
-               10 + frames[i + 1][0]
-             else # 通常フレーム
-               frames[i][0] + frames[i][1]
-             end
-           elsif frames[9][0] == 10
-             # 10フレーム目の処理（i == 9）
-             frames[9][0] + frames[9][1] + frames[9][2]
-           elsif frames[9][0] + frames[9][1] == 10
-             frames[9][0] + frames[9][1] + frames[9][2]
-           else
-             frames[9][0] + frames[9][1]
-           end
+  point += frames[i].sum # 基本点
+  next if i == 9
+
+  if frames[i][0] == 10
+    point += frames[i + 1][0]
+    point += frames[i + 1][1] || frames[i + 2][0]
+  elsif frames[i].sum == 10
+    point += frames[i + 1][0]
+  end
 end
 
 puts point
