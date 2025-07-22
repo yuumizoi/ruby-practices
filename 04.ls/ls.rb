7e48c782148957d101b01e33242978b6e8622499
yuumizoi <y.mizoi@genbasupport.com>
2025-07-22 18:28:36 +0900

    ファイル名を3列かつ左揃えに整形して表示させる処理を追加

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,20 +13,19 @@ end
 def display_files(file_names, width)
     # 行数を計算
     row_count = (file_names.size.to_f / COLUMN_COUNT).ceil
-
-    # 確認用に表示
-    puts "ファイル数： #{file_names.size.to_f}"
-    puts "必要な行数： #{row_count}"
-
     # 不足分を空文字で埋め、3列分のマス目を満たす
     padding_count = row_count * COLUMN_COUNT - file_names.size
     file_names += [""] * padding_count
 
-    # 追加された空文字が何個かを表示
-    puts "追加された空文字の数: #{padding_count}"
-
-    # 右詰め1列表示（整形前）
-    file_names.each { |name| puts name.rjust(width) }
+    row_count.times do |row_index|
+        # puts row_index
+        row = []
+        COLUMN_COUNT.times do |col_index|
+            index = row_index + row_count * col_index
+            row << file_names[index].ljust(width)
+        end
+        puts row.join('  ')
+    end
 end
 
 # ▼ ›動作確認用テストコード（開発後に削除予定）
