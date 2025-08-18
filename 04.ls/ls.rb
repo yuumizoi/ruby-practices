73f5722de9d72ea4a7b6af6d02ee2c7c2ca623a2
yuumizoi <y.mizoi@genbasupport.com>
2025-08-18 15:57:17 +0900

    RuboCopからの指摘に従い、行配列の生成をtimes.mapから Array.new(COLUMN_COUNT)へ書き換え

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -18,7 +18,7 @@ def display_files(file_names, width)
   file_names += [''] * padding_count
 
   row_count.times do |row_index|
-    row = COLUMN_COUNT.times.map do |col_index|
+    row = Array.new(COLUMN_COUNT) do |col_index|
       index = row_index + row_count * col_index
       file_names[index].ljust(width)
     end
