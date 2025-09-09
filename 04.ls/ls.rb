531fdfacdb50fd41e201e6c33c274ca8fc404bcd
yuumizoi <y.mizoi@genbasupport.com>
2025-09-09 14:13:46 +0900

    ls4: 出力部をメソッド化／-l 0件時は total のみで終了

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -48,6 +48,53 @@ def format_mtime_for_ls(time, _now = Time.now)
   time.strftime('%-m %e %H:%M')
 end
 
+def print_output(file_names, long)
+  if long
+    stats = file_names.map { |n| [n, File.lstat(n)] }
+    total_blocks = stats.sum { |_, st| st.blocks.nil? ? ((st.size + 511) / 512) : st.blocks }
+    puts "total #{total_blocks}"
+    return if stats.empty?
+    
+    widths = {
+      link: [1, *stats.map { |_, st| st.nlink.to_s.size }].max,
+      user: [1, *stats.map { |_, st| (Etc.getpwuid(st.uid)&.name || st.uid.to_s).size }].max,
+      group: [1, *stats.map { |_, st| (Etc.getgrgid(st.gid)&.name || st.gid.to_s).size }].max,
+      size: [1, *stats.map { |_, st| st.size.to_s.size }].max,
+      mtime: [1, *stats.map { |_, st| format_mtime_for_ls(st.mtime).size }].max
+    }
+    
+    stats.each do |name, st|
+      user  = Etc.getpwuid(st.uid)&.name || st.uid.to_s
+      group = Etc.getgrgid(st.gid)&.name || st.gid.to_s
+
+      type_char = file_type_char(st)
+      m = st.mode & 0o777
+      perms = [
+        (m & 0o400).zero? ? '-' : 'r', (m & 0o200).zero? ? '-' : 'w', (m & 0o100).zero? ? '-' : 'x',
+        (m & 0o040).zero? ? '-' : 'r', (m & 0o020).zero? ? '-' : 'w', (m & 0o010).zero? ? '-' : 'x',
+        (m & 0o004).zero? ? '-' : 'r', (m & 0o002).zero? ? '-' : 'w', (m & 0o001).zero? ? '-' : 'x'
+      ].join
+      permstr = type_char + perms
+  
+      # 拡張属性が付いている場合は @ を追加（macOS）
+      begin
+        xattr_output = `xattr -l -- #{Shellwords.escape(name)} 2>/dev/null`
+        has_xattr = !xattr_output.strip.empty?
+      rescue StandardError
+        has_xattr = false
+      end
+      permstr += '@' if has_xattr
+  
+      mtime = format_mtime_for_ls(st.mtime)
+      printf "%-11s %#{widths[:link]}d %-#{widths[:user]}s  %-#{widths[:group]}s  %#{widths[:size]}d  %-#{widths[:mtime]}s %s\n",
+              permstr, st.nlink, user, group, st.size, mtime, name
+    end
+  else
+    width = name_column_width(file_names)
+    display_files(file_names, width)
+  end
+end
+
 # ---- options ----
 all = false
 reverse = false
@@ -63,46 +110,4 @@ file_names = fetch_visible_files(all:)
 file_names.reverse! if reverse
 
 # ---- output ----
-if long
-  stats = file_names.map { |n| [n, File.lstat(n)] }
-  total_blocks = stats.sum { |_, st| st.blocks.nil? ? ((st.size + 511) / 512) : st.blocks }
-  puts "total #{total_blocks}"
-  widths = {
-    link: [1, *stats.map { |_, st| st.nlink.to_s.size }].max,
-    user: [1, *stats.map { |_, st| (Etc.getpwuid(st.uid)&.name || st.uid.to_s).size }].max,
-    group: [1, *stats.map { |_, st| (Etc.getgrgid(st.gid)&.name || st.gid.to_s).size }].max,
-    size: [1, *stats.map { |_, st| st.size.to_s.size }].max,
-    mtime: [1, *stats.map { |_, st| format_mtime_for_ls(st.mtime).size }].max
-  }
-
-  stats.each do |name, st|
-    user  = Etc.getpwuid(st.uid)&.name || st.uid.to_s
-    group = Etc.getgrgid(st.gid)&.name || st.gid.to_s
-
-    type_char = file_type_char(st)
-
-    m = st.mode & 0o777
-    perms = [
-      (m & 0o400).zero? ? '-' : 'r', (m & 0o200).zero? ? '-' : 'w', (m & 0o100).zero? ? '-' : 'x',
-      (m & 0o040).zero? ? '-' : 'r', (m & 0o020).zero? ? '-' : 'w', (m & 0o010).zero? ? '-' : 'x',
-      (m & 0o004).zero? ? '-' : 'r', (m & 0o002).zero? ? '-' : 'w', (m & 0o001).zero? ? '-' : 'x'
-    ].join
-    permstr = type_char + perms
-
-    # 拡張属性が付いている場合は @ を追加（macOS）
-    begin
-      xattr_output = `xattr -l -- #{Shellwords.escape(name)} 2>/dev/null`
-      has_xattr = !xattr_output.strip.empty?
-    rescue StandardError
-      has_xattr = false
-    end
-    permstr += '@' if has_xattr
-
-    mtime = format_mtime_for_ls(st.mtime)
-    printf "%-11s %#{widths[:link]}d %-#{widths[:user]}s  %-#{widths[:group]}s  %#{widths[:size]}d  %-#{widths[:mtime]}s %s\n",
-           permstr, st.nlink, user, group, st.size, mtime, name
-  end
-else
-  width = name_column_width(file_names)
-  display_files(file_names, width)
-end
+print_output(file_names, long)
