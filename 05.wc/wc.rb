76909c1c53544da868d7b09d0b98260ae5a11394
yuumizoi <y.mizoi@genbasupport.com>
2025-09-24 22:39:07 +0900

    wc: 複数ファイルに対応（各ファイルを出力し、最後にtotalを追加）

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,26 +4,59 @@
 require 'optparse'
 
 def parse_options
-    opt = OptionParser.new
-    options = {}
-    opt.on('-l', 'count lines') { options[:l] = true }
-    opt.on('-w', 'count words') { options[:w] = true }
-    opt.on('-c', 'count bytes') { options[:c] = true }
-    opt.parse!(ARGV)
-    options
+  opt = OptionParser.new
+  options = {}
+  opt.on('-l', 'count lines'){ options[:l] = true }
+  opt.on('-w', 'count words'){ options[:w] = true }
+  opt.on('-c', 'count bytes'){ options[:c] = true }
+  opt.parse!(ARGV)
+  options
 end
 
 opts = parse_options
 opts = { l: true, w: true, c: true } unless opts.values.any?
 pipe_input = ARGV.empty? && !STDIN.tty?
 
-text = STDIN.read if pipe_input
-name = nil if pipe_input
-text = File.read(ARGV.first, mode: 'rb') unless pipe_input
-name = ARGV.first unless pipe_input
-lines = text.count("\n")
-words = text.scan(/\S+/).length
-bytes = text.bytesize
+if pipe_input
+  # --- STDIN：1回だけ処理 ---
+  text  = STDIN.read
+  lines = text.count("\n")
+  words = text.scan(/\S+/).length
+  bytes = text.bytesize
 
-cols=[]; cols<<lines if opts[:l]; cols<<words if opts[:w]; cols<<bytes if opts[:c]
-puts(name ? "#{cols.join(' ')} #{name}" : cols.join(' '))
+  cols = []
+  cols << lines if opts[:l]
+  cols << words if opts[:w]
+  cols << bytes if opts[:c]
+  puts cols.join(' ')
+else
+  # --- 複数ファイル：各ファイル＋最後に total ---
+  total_l = 0
+  total_w = 0
+  total_c = 0
+
+  ARGV.each do |path|
+    t = File.read(path, mode: 'rb')
+    l = t.count("\n")
+    w = t.scan(/\S+/).length
+    c = t.bytesize
+
+    total_l += l
+    total_w += w
+    total_c += c
+
+    cols = []
+    cols << l if opts[:l]
+    cols << w if opts[:w]
+    cols << c if opts[:c]
+    puts "#{cols.join(' ')} #{path}"
+  end
+
+  if ARGV.size >= 2
+    cols = []
+    cols << total_l if opts[:l]
+    cols << total_w if opts[:w]
+    cols << total_c if opts[:c]
+    puts "#{cols.join(' ')} total"
+  end
+end
