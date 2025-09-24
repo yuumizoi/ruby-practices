f745fffe7be0bf88fd820af984756a91c170aaf8
yuumizoi <y.mizoi@genbasupport.com>
2025-09-24 16:08:13 +0900

    wc: 入力の判定と読み込みを実装

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -13,3 +13,8 @@ end
 
 opts = parse_options
 pipe_input = ARGV.empty? && !STDIN.tty?
+
+text = STDIN.read if pipe_input
+name = nil if pipe_input
+text = File.read(ARGV.first, mode: 'rb') unless pipe_input
+name = ARGV.first unless pipe_input
