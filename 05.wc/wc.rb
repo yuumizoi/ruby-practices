5dd9dceb3cb9aaef143c63a50704caeb27e80a9d
yuumizoi <y.mizoi@genbasupport.com>
2026-02-10 13:50:03 +0900

    wc: 表示項目のキーと幅を個別の配列で管理していた箇所をHashに集約し、保守性を向上

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -43,10 +43,12 @@ rows << { l: total_l, w: total_w, c: total_c, name: 'total' } if input_files.siz
 
 enabled_metric_keys = %i[l w c].select { |k| opts[k] }
 COLUMN_WIDTH = 8
-widths = Array.new(enabled_metric_keys.size, COLUMN_WIDTH)
+metrics_with_width = enabled_metric_keys.to_h { |key| [key, COLUMN_WIDTH] }
 
 rows.each do |row|
-  formatted_columns = enabled_metric_keys.map.with_index { |k, i| row[k].to_s.rjust(widths[i]) }
+  formatted_columns = metrics_with_width.map do |key, width|
+    row[key].to_s.rjust(width)
+  end
   formatted_row = formatted_columns.join
   puts [formatted_row, row[:name]].compact.join(' ')
 end
