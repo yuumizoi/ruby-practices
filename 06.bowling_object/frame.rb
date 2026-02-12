9792245361ac85a52099acf374075cbf3d243b42
yuumizoi <y.mizoi@genbasupport.com>
2026-02-12 14:39:21 +0900

    feat: フレームの判定とスコア計算を管理するFrameクラスを作成

---
+++ 06.bowling_object/frame.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Frame
+  def initialize(shots)
+    @shots = shots
+  end
+
+  def strike?
+    @shots[0].pins == 10
+  end
+
+  def spare?
+    !strike? && @shots[0..1].sum { |shot| shot.pins } == 10
+  end
+
+  def score
+    @shots.sum { |shot| shot.pins }
+  end
+
+  def pins_at(index)
+    @shots[index]&.pins || 0
+  end
+end
