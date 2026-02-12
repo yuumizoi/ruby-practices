f292490638e290bec36e33aa9ee57000cf1d0535
yuumizoi <y.mizoi@genbasupport.com>
2026-02-12 16:34:40 +0900

    feat: Gameクラスの実装とbowling.rbのリファクタリング

--- 06.bowling_object/bowling.rb
+++ 06.bowling_object/bowling.rb
@@ -1,34 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative 'game'
+
 score_string = ARGV[0]
-scores = score_string.split(',')
-
-shots = scores.map { |score_string_item| score_string_item == 'X' ? 10 : score_string_item.to_i }
-
-frames = []
-index = 0
-
-while index < shots.length && frames.length < 10
-  if frames.length == 9
-    frames << shots[index..]
-    break
-  elsif shots[index] == 10
-    frames << [10]
-    index += 1
-  else
-    frames << [shots[index], shots[index + 1]]
-    index += 2
-  end
-end
-
-point = 0
-
-frames.size.times do |i|
-  point += frames[i].sum
-  next if i == 9 || frames[i].sum != 10
-
-  point += frames[i + 1][0]
-  point += frames[i + 1][1] || frames[i + 2][0] if frames[i][0] == 10
-end
-puts point
+game = Game.new(score_string)
+puts game.calculate_total_score
---
+++ 06.bowling_object/game.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require_relative 'shot'
+require_relative 'frame'
+
+class Game
+  def initialize(score_string)
+    marks = score_string.split(',')
+    @shots = marks.map { |mark| Shot.new(mark) }
+    @frames = build_frames
+  end
+
+  def calculate_total_score
+    total_score = 0
+    @frames.each_with_index do |frame, i|
+      total_score += frame.score
+      if i < 9 && (frame.strike? || frame.spare?)
+        total_score += bonus_score(i, frame.strike?)
+      end
+    end
+    total_score
+  end
+
+  private
+
+  def build_frames
+    frames = []
+    index = 0
+
+    while index < @shots.length && frames.length < 10
+      if frames.length == 9
+        frames << Frame.new(@shots[index..])
+        break
+      elsif @shots[index].pins == 10
+        frames << Frame.new([@shots[index]])
+        index += 1
+      else
+        frames << Frame.new([@shots[index], @shots[index + 1]])
+        index += 2
+      end
+    end
+    frames
+  end
+
+  def bonus_score(index, is_strike)
+    next_frame = @frames[index + 1]
+    if is_strike
+      next_frame.pins_at(0) + (next_frame.pins_at(1) || @frames[index + 2].pins_at(0))
+    else
+      next_frame.pins_at(0)
+    end
+  end
+end
+
