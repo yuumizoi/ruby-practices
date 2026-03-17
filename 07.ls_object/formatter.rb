6688a45ada52bf085060ca99d86a4d723f3fe801
yuumizoi <y.mizoi@genbasupport.com>
2026-03-17 16:35:03 +0900

    feat: データを縦並び順の2次元配列に再配置する ordered_rows メソッドを実装

--- 07.ls_object/formatter.rb
+++ 07.ls_object/formatter.rb
@@ -7,4 +7,11 @@ class Formatter
   def rows_count
     @entries.size.ceildiv(3)
   end
+  def ordered_rows
+    rows = []
+    @entries.each_slice(rows_count) do |slice|
+      rows << (slice + [nil] * (rows_count - slice.size))
+    end
+    rows.transpose
+  end
 end
