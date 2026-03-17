039716fcccff3056315fe52d4fd21f487830dc71
yuumizoi <y.mizoi@genbasupport.com>
2026-03-17 17:34:48 +0900

    feat: 列幅を揃えるための max_name_width と format_name を実装

--- 07.ls_object/formatter.rb
+++ 07.ls_object/formatter.rb
@@ -7,11 +7,21 @@ class Formatter
   def rows_count
     @entries.size.ceildiv(3)
   end
+
   def ordered_rows
     rows = []
     @entries.each_slice(rows_count) do |slice|
       rows << (slice + [nil] * (rows_count - slice.size))
     end
     rows.transpose
+  end
+
+  def max_name_width
+    @entries.map { |e| e.name.size }.max || 0
+  end
+
+  def format_name(entry)
+    return "" if entry.nil?
+    entry.name.ljust(max_name_width + 2)
   end
 end
